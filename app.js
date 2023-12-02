e62b4a096993be2834cf1b78bdd3c9b06d0d943d
Yury Kavalenka <yurykavalenka@gmail.com>
2023-12-02 10:06:25 +0100

    Data types

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
-// Порядок операторов
+// Типы данных
 
-const isSuited = 100 - 10 > 90 - 5;
-console.log(isSuited);
+// Объекты и примитивы
 
-// 100 - (12) 10 > (10) 90 - (12) 5
+ const age = 18; // Числа
+console.log(typeof age)
+
+ const surname = 'Kavalenka'; // Строки
+console.log(typeof surname);
+
+ const isAdmin = true; // true and false
+ console.log(typeof isAdmin);
+
+ const isAdmin2 = undefined; // Не заданное значение 
+let data; 
+console.log(typeof isAdmin2);
+
+let data2 = null; // Пустое значение
+console.log(typeof data2);
+
+
+const admin = Symbol('Admin'); // Уникальное неизменное значение
+console.log(typeof admin);
+
+const big = BigInt(9999999999999999999); // Работа с большими числами
+
+
