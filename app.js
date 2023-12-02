89274c35f90ea18ea64b28ff8c28faf86b446c0b
Yury Kavalenka <yurykavalenka@gmail.com>
2023-12-02 09:45:22 +0100

    Operator precedence

--- app.js
+++ app.js
@@ -1,39 +1,6 @@
-// Базовые арифмитические операторы
+// Порядок операторов
 
-const width = 10;
-const height = 5;
-const space = width * height; 
-const volume = 2 ** 3;
-console.log(space);
-console.log(volume);
+const isSuited = 100 - 10 > 90 - 5;
+console.log(isSuited);
 
-
-// Строки
-
-const city = 'Warszawa';
-const street = 'Plotska'
-console.log(city + ', ' + street + ' ' + 5); // Конкатенация
-
-
-// Операторы присваивания
-
-let age = 18;
-age += 2; // age = age + 2
-age -= 2; // age = age - 2
-age *= 2; // age = age * 2
-age /= 2; // age = age / 2
-
-age++; // age = age + 1
-age--; // age = age - 1
-
-
-console.log(age);
-
-//  Операторы сравнения
-
-const vasia = 20;
-console.log(age > vasia);
-console.log(age >= vasia);
-console.log(age < vasia);
-console.log(age <= vasia);
-console.log(age == vasia);
+// 100 - (12) 10 > (10) 90 - (12) 5
