13a97e0073346bcf6e4479044635552045831573
Yury Kavalenka <yurykavalenka@gmail.com>
2023-12-02 09:31:52 +0100

    Assignment Operators

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-console.log('Hello World!')
+// Базовые арифмитические операторы
+
+const width = 10;
+const height = 5;
+const space = width * height; 
+const volume = 2 ** 3;
+console.log(space);
+console.log(volume);
+
+
+// Строки
+
+const city = 'Warszawa';
+const street = 'Plotska'
+console.log(city + ', ' + street + ' ' + 5); // Конкатенация
+
+
+// Операторы присваивания
+
+let age = 18;
+age += 2; // age = age + 2
+age -= 2; // age = age - 2
+age *= 2; // age = age * 2
+age /= 2; // age = age / 2
+
+age++; // age = age + 1
+age--; // age = age - 1
+
+
+console.log(age);
+
+//  Операторы сравнения
+
+const vasia = 20;
+console.log(age > vasia);
+console.log(age >= vasia);
+console.log(age < vasia);
+console.log(age <= vasia);
+console.log(age == vasia);
