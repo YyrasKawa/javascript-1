de44cabfb8c7826071b70301101fda6fe29fdfa9
Yury Kavalenka <yurykavalenka@gmail.com>
2023-12-02 10:40:59 +0100

    Template strings

--- app.js
+++ app.js
@@ -1,27 +1,19 @@
-/*
-Ваша часовая ставка 80$ и вы готовы работать не
-более 5 часов в день 5 дней в неделю (кроме выходных).
-К вам приходит заказчик и предлагает заказ на 40
-часов работы.
-Сейчас понедельник.
-Вы должны уехать через 11 дней.
-Выведете в консоль:
-- Boolean переменную успеете ли вы взяться за работу
-- Сколько вы за неё попросите?
-*/
+// Шаблонные строки
 
-// Моё решение
+const projectName = 'Site shop';
+const price = 2000;
+const author = 'Vasilij Pupkin';
+const template = author + ' заказал ' + projectName + ' по цене ' + price + '$';
+console.log(template); 
 
-const hourWeek = 5 * 5;
-const workDays = 11 - 2;
-const workDaysOrder = 40 / 5;
-console.log(workDays >= workDaysOrder);
-console.log(40 * 80);
 
-// Решение
+const template2 = `${author} заказал ${projectName} по цене ${price}$`;
+console.log(template2); 
 
-const payRateUSD = 80;
-const projectHours = 40;
-const availableHours = (11 - 2) * 5;
-console.log("Смогу ли я работать " + (availableHours > projectHours));
-console.log("Стоимость работ: " + projectHours * payRateUSD + "$");
+const template3 = 'Project \n' + 'Price: ' + price + '$';
+console.log(template3);
+
+const template4 = `Project
+Price: ${price}$`
+console.log(template4);
+
