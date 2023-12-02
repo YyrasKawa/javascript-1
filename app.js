e0b172a3ba8a3e80736d8d51f0d6495e96b2a5de
Yury Kavalenka <yurykavalenka@gmail.com>
2023-12-02 10:26:17 +0100

    First task

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-// Типы данных
+/*
+Ваша часовая ставка 80$ и вы готовы работать не
+более 5 часов в день 5 дней в неделю (кроме выходных).
+К вам приходит заказчик и предлагает заказ на 40
+часов работы.
+Сейчас понедельник.
+Вы должны уехать через 11 дней.
+Выведете в консоль:
+- Boolean переменную успеете ли вы взяться за работу
+- Сколько вы за неё попросите?
+*/
 
-// Объекты и примитивы
+// Моё решение
 
- const age = 18; // Числа
-console.log(typeof age)
+const hourWeek = 5 * 5;
+const workDays = 11 - 2;
+const workDaysOrder = 40 / 5;
+console.log(workDays >= workDaysOrder);
+console.log(40 * 80);
 
- const surname = 'Kavalenka'; // Строки
-console.log(typeof surname);
+// Решение
 
- const isAdmin = true; // true and false
- console.log(typeof isAdmin);
-
- const isAdmin2 = undefined; // Не заданное значение 
-let data; 
-console.log(typeof isAdmin2);
-
-let data2 = null; // Пустое значение
-console.log(typeof data2);
-
-
-const admin = Symbol('Admin'); // Уникальное неизменное значение
-console.log(typeof admin);
-
-const big = BigInt(9999999999999999999); // Работа с большими числами
-
-
+const payRateUSD = 80;
+const projectHours = 40;
+const availableHours = (11 - 2) * 5;
+console.log("Смогу ли я работать " + (availableHours > projectHours));
+console.log("Стоимость работ: " + projectHours * payRateUSD + "$");
