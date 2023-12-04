0ced3d655c0b19e7a5bed14c6557c4b0f4824660
Yury Kavalenka <yurykavalenka@gmail.com>
2023-12-04 10:00:27 +0100

    add exercise

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
-const money = 10;
-const canBuy = money > 50;
+const moneyBank = 12000 * (1 + 0.07 / 12) ** 24;
+const costHouse = 13500;
 
- if (canBuy) {
-    console.log('Может купить наш продукт');
- } else if (money > 5) {
-    console.log('Куплен мини продукт');
- } else {
-    console.log('Не хватает баланса');
- }
+if (moneyBank >= costHouse) {
+    console.log(`Вася сможет купить дом за ${costHouse}$`)
+} else {
+    console.log(`Вася не сможет купить дом за ${costHouse}$`);
+}
+
+
+console.log(`У Васи есть ${moneyBank}$`);
+
+// -------------------------------
+
+const deposit = 12000;
+const rate = 0.07; 
+const depositLength = 24;
+const houseCost = 13500;
+const res = deposit * (1 + rate / 12) ** 24;
+if (res > houseCost) {
+    console.log(`Мы накопили: ${res}. Можем купить. Остаток ${res - houseCost}$`)
+} else {
+    console.log(`Купить не сможем `)
+}
