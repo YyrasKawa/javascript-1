be34d0ee4776db19d95dd158776ac0f4e507ac87
Yury Kavalenka <yurykavalenka@gmail.com>
2023-12-04 09:40:13 +0100

    add two task - if else

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const money = 100;
+const money = 10;
 const canBuy = money > 50;
 
  if (canBuy) {
     console.log('Может купить наш продукт');
+ } else if (money > 5) {
+    console.log('Куплен мини продукт');
  } else {
     console.log('Не хватает баланса');
-
  }
