05016b182c062d58bcfbd77041c1a2b4518e1c21
Yury Kavalenka <yurykavalenka@gmail.com>
2023-12-22 19:02:43 +0100

    Exercise - URL Parsing Function

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-const data = [1, 2, 3, 4, 5, 6];
-const [one, two, ...others] = data;
-console.log(one, two, others);
+const url = 'https://purpleschool.ru/course/javascript';
+
+// const res = url.split('/');
+// console.log(res);
+
+
+function getUrlParts(url) {
+    const [protocol, _, host, ...path] = url.split('/');
+    if (protocol === 'https:' || protocol === 'http:') {
+        console.log(protocol, _, host, path);
+        console.log(`Protocol: ${protocol.split(":")[0]}`);
+        console.log(`Host: ${host}`);
+        console.log(`Site wayl: /${path.join("/")}`);
+    }
+}
+
+getUrlParts(url);
