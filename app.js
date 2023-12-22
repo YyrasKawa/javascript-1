18ce117c005afbcb54a1a204d7765c5c6463a890
Yury Kavalenka <yurykavalenka@gmail.com>
2023-12-22 12:45:14 +0100

    Functions within functions

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-function canAccessWebsite(age) {
-    if (age < 18) {
-        return false;
-    } 
-        return true;
+const KG_IN_USD = 7;
+const KM_IN_USD = 5;
+
+function calculateW(present) {
+    return present * KG_IN_USD;
 }
 
-console.log(canAccessWebsite(16));
 
-const  canAccessWebsite2 = age => age < 18 ? 'No' : 'Yes';
-console.log(canAccessWebsite2(16));
+function calculateKm(distance) {
+  return distance * KM_IN_USD;
+}
+
+function getExchagePrice(present1, present2, distance) {
+    const price1 = calculateW(present1);
+    const price2 = calculateW(present2);
+    const distancePrice = calculateKm(distance);
+    return price1 + price2 + distancePrice;
+}
+
+console.log(getExchagePrice(1,2,10))
