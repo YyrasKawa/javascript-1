4c12f09c8d053d954bd07e6f3bdd61ff44ffe6bf
Yury Kavalenka <yurykavalenka@gmail.com>
2023-12-22 16:03:32 +0100

    Search for an element

--- app.js
+++ app.js
@@ -1,26 +1,16 @@
-const users = ['Ana', 'Victoria', 'Kate'];
-console.log(users);
-users[2] = 'Sonia';
-console.log(users);
-users[3] = 'Maxim';
-console.log(users);
-users[5] = 'Titan';
-console.log(users.length);
+const roles = ['user', 'admin', 'manager'];
 
-const arrLenght = users.push('Nikita');
-console.log(users);
-console.log(arrLenght);
-
-users.unshift('Vasia');
-console.log(users);
-
-const el = users.pop();
-console.log(users);
-console.log(el);
-
-users.shift();
-console.log(users);
+console.log(roles.indexOf('admin'));
 
 
 
+if (roles.indexOf('admin') >= 0) {
+    console.log('Yes')
+}
 
+console.log(roles.includes('admin'));
+console.log(roles.includes("superuser"));
+
+if (roles.includes("admin")) {
+  console.log("Yes");
+}
