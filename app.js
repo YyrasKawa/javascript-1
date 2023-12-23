e3f694d509aefb3ecb02abf08ad8126e0faf1e04
Yury Kavalenka <yurykavalenka@gmail.com>
2023-12-23 08:39:41 +0100

    Loop within a Loop

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
-const arr = ['!', 'JS', 'love', 'I'];
-const newArr = [];
+const tasks = [[1, 'Task 1'], [2, 'Task 2']];
 
-for (let i = arr.length -1; i >= 0; i--) {
-     newArr.push(arr[i]);
-     console.log(arr[i]);
- }
-
-
- 
- console.log(newArr);
- const myString =   newArr.join(' ');
- console.log(myString);
-
+for (let i = 0; i < tasks.length; i++) {
+    for(let j = 0; j < tasks[i].length; j++) {
+        console.log(tasks[i][j]);
+    }
+}
