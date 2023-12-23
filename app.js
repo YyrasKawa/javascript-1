06fecf8579ae255b04e7c59a5380ff58285ca8c3
Yury Kavalenka <yurykavalenka@gmail.com>
2023-12-23 15:34:31 +0100

    sort

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
-const arr = [2, 4, 4, 10, 20];
+const users = ['Yury', 'Mari', 'Alex', 'Anna'];
+console.log(users);
+users.sort();
+console.log(users);
 
-// function some(array, element) {
-//     if (array.find(el => el === element)) {
-//         return true;
-//     } else {
-//         return false;
-//     }
+const operations = [100, -300, -100, 50, 480];
+operations.sort((a, b) => {
+    if (a > b) {
+        return 1;
+    }
+    if (a < b) {
+        return -1;
+    }
+})
+console.log(operations);
 
-// }
-
-// console.log(some(arr, 5));
-
-function some(array, element) {
-    const res = array.find(el => el === element);
-    return res == undefined ? false : true;
-}
-
-console.log(some(arr, 0));
-
-console.log(arr.some(el => el === 0));
+operations.sort((a, b) => b - a);
+console.log(operations)
