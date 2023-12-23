33cc72a46943ee57658823cb97802578b4b1761a
Yury Kavalenka <yurykavalenka@gmail.com>
2023-12-23 10:45:29 +0100

    Exercise - Calculating the final balance

--- app.js
+++ app.js
@@ -1,13 +1,83 @@
-const arr = [1, 2, 8, 7];
+const operations = [1000, -700, 300, -500, 10000];
+const startingBalance = 100;
 
-for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
+function getBalance(arrayOfOperations, initailBalance) {
+    let balance = initailBalance;
+    for (const element of arrayOfOperations) {
+        balance += element;
+    }
+    return balance;
 }
 
-for (let element of arr) {
-    console.log(element);
+console.log(getBalance(operations, startingBalance));
+
+function checkOperations(arrayOfOperations, initailBalance) {
+    let balance = initailBalance;
+    let isOk = true;
+    for ( const element of arrayOfOperations) {
+        balance += element; 
+        if (balance < 0) {
+            isOk = false;
+            break
+        }
+    }
+    return isOk;
 }
 
-for (let index in arr) {
-    console.log(index)
+console.log(checkOperations(operations, startingBalance));
+
+function avarageOperations(arrayOfOperations) {
+    let positiveCount = 0;
+    let positiveSum = 0;
+    let negativeCount = 0;
+    let negativeSum = 0;
+    for (const element of arrayOfOperations) {
+        if (element > 0) {
+            positiveCount++;
+            positiveSum += element;
+        }
+        if (element < 0) {
+          negativeCount++;
+          negativeSum += element;
+        }
+  }
+  return [positiveSum / positiveCount, negativeSum / negativeCount]
+  
 }
+
+console.log(avarageOperations(operations));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const resultBalance = function(balance) {
+//     for (let element of operations) {
+//     balance = balance + element;
+//     if (balance < 0){
+//         console.log(`False ${balance}`)
+//     }
+//     }
+//     return balance
+// }
+// const balance = resultBalance(100);
+
+// console.log(resultBalance(100));
+// const midleBalanse = function(result) {
+//     for (let index in operations) {
+//         result = balance / index;
+//     }
+//     return result
+// }
+
+// console.log(midleBalanse(balance));
