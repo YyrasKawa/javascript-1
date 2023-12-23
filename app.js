2e988a40af5b1791b9598e262f2156e079bceee2
Yury Kavalenka <yurykavalenka@gmail.com>
2023-12-23 14:26:44 +0100

    find и findIndex

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-const arr = [2, 4, 4, 10];
+const arr = [2, 4, 4, 10, 20];
 
-const average = arr.reduce((acc, value, i) => {
-    if (i != arr.length - 1) {
-        return acc + value;
-    } else {
-        return (acc + value) / arr.length
-    }
-}, 0);
+let elGT5;
+// for(const el of arr) {
+//     if (el > 5) {
+//         elGT5 = el;
+//         break;
+//     }
+// }
 
-console.log(average);
+// console.log(elGT5);
+
+elGT5 = arr.find(el => el > 5);
+console.log(elGT5);
+
+elGT5Index = arr.findIndex((el) => el > 5);
+console.log(elGT5Index);
