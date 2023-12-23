4737c67b52cf561f38cedafbcb1dcc64b2a2b92b
Yury Kavalenka <yurykavalenka@gmail.com>
2023-12-23 20:59:33 +0100

    Exercise - wallet

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
-const user = 
-  {
-    name: "Yury",
-    surname: "Kavalenka",
-    age: 24,
-    getFullName: function () {
-        return this.name + ' ' + this.surname;
+const wallet = {
+    balance: 0,
+    operations: [],
+    increase: function(sum, reason) {
+        this.balance += sum;
+        this.operations.push({
+            reason: reason,
+            sum: sum
+        })
+        return true;
+    },
+    decrease: function(sum, reason) {
+        if (this.balance < sum) {
+            console.log('Недостаточно баланса');
+            return false;
+        }
+        this.balance -= sum;
+        this.operations.push({
+          reason: reason,
+          sum: -sum,
+        });
+        return true;
+    },
+    getOperationLenght: function() {
+        return this.operations.length;
     }
-  };
+};
 
-  console.log(user.getFullName());
+console.log(wallet.increase(1000, 'Зарплата'));
+console.log(wallet.getOperationLenght());
+console.log(wallet.decrease(2000, 'Покупка'));
+console.log(wallet.getOperationLenght());
+console.log(wallet.decrease(500, "Покупка телефона"));
+console.log(wallet.balance);
+console.log(wallet.operations);
+
+
+
 
 
