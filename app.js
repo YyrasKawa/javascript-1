b99af7f190f98869ee3338d68b518109c85aee4a
Yury Kavalenka <yurykavalenka@gmail.com>
2023-12-23 17:12:47 +0100

    String conversion2

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
-// const userName = 'Yury Kavalenka';
-
-// console.log(userName.charAt(2));
-// console.log(userName.length);
-// console.log(userName.indexOf('a'));
-// console.log(userName.lastIndexOf('a'));
-// console.log(userName.includes('ava'));
-
-// console.log(userName.slice(5));
+const str = 'Yury Kavalenka';
+console.log(str.toLowerCase());
+console.log(str.toUpperCase());
+console.log(str.toUpperCase());
+console.log(str.replaceAll("a", "o"));
+console.log(str.replace(/a/g, "o"));
 
 
-const userName = 'Вася aka Terminator perdinator Пупкин';
-console.log(userName.indexOf(' '));
-console.log(userName.lastIndexOf(' '));
-console.log(userName.length);
-const namefirst = userName.slice(0, 4);
-const nameSecond = userName.slice(30, 37);
-console.log(`${namefirst} ${nameSecond}`)
+const str2 = " Yury Kavalenka  \n";
+console.log(str2.trimStart());
+
+
+
 
 
 
