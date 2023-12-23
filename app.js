6e7f45f276066e61b9d6ae1535df4e8b266ee284
Yury Kavalenka <yurykavalenka@gmail.com>
2023-12-23 16:24:14 +0100

    from

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
-const users = ['Yury', 'Mari', 'Alex', 'Anna'];
-console.log(users);
-users.sort();
-console.log(users);
+const arr = [1, 2, 3, 4, 5];
 
-const operations = [100, -300, -100, 50, 480];
-operations.sort((a, b) => {
-    if (a > b) {
-        return 1;
-    }
-    if (a < b) {
-        return -1;
-    }
-})
-console.log(operations);
+const arr2 = new Array(5);
 
-operations.sort((a, b) => b - a);
-console.log(operations)
+console.log(arr2);
+arr2.fill(1, 0, 3);
+arr2.fill(2, 3, 5);
+console.log(arr2);
+
+const arr3 = Array.from({ length: 5}, (cur, i) => i + 1);
+console.log(arr3);
