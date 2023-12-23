7cafe937dfec3579f213573b545af818ed72f5c1
Yury Kavalenka <yurykavalenka@gmail.com>
2023-12-23 16:57:36 +0100

    String conversion

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-const userName = 'Yury Kavalenka';
+// const userName = 'Yury Kavalenka';
 
-console.log(userName.charAt(2));
+// console.log(userName.charAt(2));
+// console.log(userName.length);
+// console.log(userName.indexOf('a'));
+// console.log(userName.lastIndexOf('a'));
+// console.log(userName.includes('ava'));
+
+// console.log(userName.slice(5));
+
+
+const userName = 'Вася aka Terminator perdinator Пупкин';
+console.log(userName.indexOf(' '));
+console.log(userName.lastIndexOf(' '));
 console.log(userName.length);
-console.log(userName.indexOf('a'));
-console.log(userName.lastIndexOf('a'));
-console.log(userName.includes('ava'));
+const namefirst = userName.slice(0, 4);
+const nameSecond = userName.slice(30, 37);
+console.log(`${namefirst} ${nameSecond}`)
 
-console.log(userName.slice(5));
+
 
