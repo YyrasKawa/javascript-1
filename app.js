e343df20e6a36bbe9305912635db3135c93a877d
Yury Kavalenka <yurykavalenka@gmail.com>
2023-12-23 19:11:52 +0100

    Accessing Elements

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ const userArray = ['Yury', 'Kavalenka', 41];
 const user = {
     name: 'Yury',
     surname: 'Kavalenka',
-    age: 24;
+    age: 24,
+    skills: [
+        'Cooking',
+        'Development'
+    ]
+    
 }
 
-
+console.log(user)
+console.log(user.name)
+console.log(user.skills[0]);
+console.log(user['skills']);
 
 
 
