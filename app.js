e0c1b738939207f7801238f3e0b733b006c2840e
Yury Kavalenka <yurykavalenka@gmail.com>
2023-12-23 12:19:52 +0100

    map

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-const score = [5, 10, 0, 15];
+const transactionInUSD = [10, -7, 50, -10, 100];
 
-// for (const [i, el] of score.entries()) {
-//     console.log(`Раунд: ${i + 1}: ${el}`)
-// }
+const transactionInRUB = [];
+for (const transaction of transactionInUSD) {
+    transactionInRUB.push(transaction * 60);
+}
 
+console.log(transactionInUSD);
+console.log(transactionInRUB);
 
-score.forEach((el, i) => {
-    console.log(`Раунд ${i + 1}: ${el}`)
-})
+const transactionInRUB2 = transactionInUSD.map((transaction) => transaction * 60);
+
+console.log(transactionInUSD);
+console.log(transactionInRUB2);
