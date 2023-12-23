baa58998c973c60dff06bcf16787f2d859d45a96
Yury Kavalenka <yurykavalenka@gmail.com>
2023-12-23 14:16:32 +0100

    Exercise - Average

--- app.js
+++ app.js
@@ -1,28 +1,11 @@
-const operations = [100, -20, 7, -30, 50];
+const arr = [2, 4, 4, 10];
 
-// let balance = 0;
-// for (const operation of operations) {
-//     balance += operation;
-// }
-
-// console.log(balance);
-
-// const finalBalance = operations.reduce((acc, value) => {
-//     console.log(`acc ${acc} value ${value}`);
-//     return acc += value;
-// }, 0)
-
-// const finalBalance2 = operations.reduce((acc, value) => acc += value, 0);
-
-// console.log(finalBalance2);
-
-
-const minElement = operations.reduce((acc, operation) => {
-  if (operation > acc) {
-    return acc;
-  } else {
-    return operation
-  }
+const average = arr.reduce((acc, value, i) => {
+    if (i != arr.length - 1) {
+        return acc + value;
+    } else {
+        return (acc + value) / arr.length
+    }
 }, 0);
 
-console.log(minElement)
+console.log(average);
