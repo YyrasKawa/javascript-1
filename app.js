0d984187c05a5caf2834fcfefab745d8bf240541
Yury Kavalenka <yurykavalenka@gmail.com>
2023-12-23 17:37:59 +0100

    Exercise - checking the number

--- app.js
+++ app.js
@@ -1,13 +1,45 @@
-const str = 'Yury Kavalenka';
-console.log(str.toLowerCase());
-console.log(str.toUpperCase());
-console.log(str.toUpperCase());
-console.log(str.replaceAll("a", "o"));
-console.log(str.replace(/a/g, "o"));
+const num1 = '89103235356';
+const num2 = '+79103235356';
+const num3 = '+7(910)3235356';
+const num4 = '+7(910)3235356';
+const num5 = " +7(910) 323-53-56 ";
 
+const num6 = "891032353";
+const num7 = "+7d910323-53-56";
+const num8 = "9+7103235356";
 
-const str2 = " Yury Kavalenka  \n";
-console.log(str2.trimStart());
+function isPhoneNumber(num) {
+    num = num.trim();
+    num = num.replace('+7', '8');
+    if (!num.startsWith('8')) {
+        return false;
+    }
+
+    num = num.replaceAll('(', '');
+    num = num.replaceAll(')', '');
+    num = num.replaceAll(' ', '');
+    num = num.replaceAll('-', '');
+    if (num.length !=11) {
+        return false;
+    }
+    let onlyNumber = true;
+    for (const char of num) {
+        if (isNaN(Number(char))) {
+            onlyNumber = false;
+            break;
+        }
+    }
+    return onlyNumber;
+}
+
+console.log(isPhoneNumber(num1));
+console.log(isPhoneNumber(num2));
+console.log(isPhoneNumber(num3));
+console.log(isPhoneNumber(num4));
+console.log(isPhoneNumber(num5));
+console.log(isPhoneNumber(num6));
+console.log(isPhoneNumber(num7));
+console.log(isPhoneNumber(num8));
 
 
 
