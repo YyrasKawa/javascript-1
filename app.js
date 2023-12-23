a47ab7c94df1789158b7452c280487600d5eeb7f
Yury Kavalenka <yurykavalenka@gmail.com>
2023-12-23 16:41:14 +0100

    Basic Methods Working with Strings

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-const arr = [1, 2, 3, 4, 5];
+const userName = 'Yury Kavalenka';
 
-const arr2 = new Array(5);
+console.log(userName.charAt(2));
+console.log(userName.length);
+console.log(userName.indexOf('a'));
+console.log(userName.lastIndexOf('a'));
+console.log(userName.includes('ava'));
 
-console.log(arr2);
-arr2.fill(1, 0, 3);
-arr2.fill(2, 3, 5);
-console.log(arr2);
+console.log(userName.slice(5));
 
-const arr3 = Array.from({ length: 5}, (cur, i) => i + 1);
-console.log(arr3);
