5412517314903526dc63bad95d055c6e256c10b4
Yury Kavalenka <yurykavalenka@gmail.com>
2023-12-24 17:46:55 +0100

    Setting Attributes

--- app.js
+++ app.js
@@ -16,9 +16,11 @@ function submitForm() {
   }
   document.querySelector(".panel").innerText = input;
   document.querySelector(".input").value = "";
-  document
-    .querySelector(".notification")
-    .classList.remove("notification_hidden");
+//   document
+//     .querySelector(".notification")
+//     .classList.remove("notification_hidden");
+document.querySelector('.notification').getAttribute('class');
+document.querySelector('.notification').setAttribute('class', 'notification');
 }
 
 function inputChaged(e) {
