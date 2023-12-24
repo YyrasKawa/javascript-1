23fa91b110326759a5a6289848c546fe9072ac24
Yury Kavalenka <yurykavalenka@gmail.com>
2023-12-24 07:37:05 +0100

    Iterating over an object

--- app.js
+++ app.js
@@ -1,40 +1,23 @@
-const wallet = {
-    balance: 0,
-    operations: [],
-    increase: function(sum, reason) {
-        this.balance += sum;
-        this.operations.push({
-            reason: reason,
-            sum: sum
-        })
-        return true;
+const cities = {
+    msk: {
+        let: 200,
+        temp: 25
     },
-    decrease: function(sum, reason) {
-        if (this.balance < sum) {
-            console.log('Недостаточно баланса');
-            return false;
-        }
-        this.balance -= sum;
-        this.operations.push({
-          reason: reason,
-          sum: -sum,
-        });
-        return true;
-    },
-    getOperationLenght: function() {
-        return this.operations.length;
+    spb: {
+        lt: 100,
+        temp: 20
     }
-};
+}
 
-console.log(wallet.increase(1000, 'Зарплата'));
-console.log(wallet.getOperationLenght());
-console.log(wallet.decrease(2000, 'Покупка'));
-console.log(wallet.getOperationLenght());
-console.log(wallet.decrease(500, "Покупка телефона"));
-console.log(wallet.balance);
-console.log(wallet.operations);
+let sumTemp = 0;
+let citiesCount = Object.keys(cities).length;
+// for (const key in cities) {
+//     console.log(key);
+//     sumTemp += cities[key].temp;
+// }
 
+for (const key of Object.keys(cities)) {
+    sumTemp += cities[key].temp;
+}
 
-
-
-
+console.log(sumTemp / citiesCount);
