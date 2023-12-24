4a1bcdf37cbc31c5721d8a9da23044c1abbb8cae
Yury Kavalenka <yurykavalenka@gmail.com>
2023-12-24 14:36:44 +0100

    IIFE

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
 'use strict'
 
-function removePassword(reset) {
-    if (reset) {
-        this.password = undefined;
-    } else {
-        this.password = '1';
-    }
+// IIFE
+
+function init() {
+    console.log('Start');
 }
+init();
 
-const user = {
-    login: 'a@a.pl',
-    password: '12345'
-};
-
-const resetUserPassword = removePassword.bind(user, true);
-resetUserPassword();
-console.log(user);
+(function() {
+    console.log('Start IIFO')
+})();
 
