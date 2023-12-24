5047da973b1b1ca10cfb38ea25e1b95b309d8ab1
Yury Kavalenka <yurykavalenka@gmail.com>
2023-12-24 17:59:22 +0100

    Exercise - getting DOM elements

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
 "use strict";
 
-// document.querySelector('.button').addEventListener('click', function() {
-//     const input = document.querySelector('.input').value;
-//     if (!input) {
-//         return;
-//     }
-//     document.querySelector('.panel').innerText = input;
-//     document.querySelector('.input').value = '';
-// });
-
 function submitForm() {
   const input = document.querySelector(".input").value;
   if (!input) {
@@ -16,15 +7,22 @@ function submitForm() {
   }
   document.querySelector(".panel").innerText = input;
   document.querySelector(".input").value = "";
-//   document
-//     .querySelector(".notification")
-//     .classList.remove("notification_hidden");
-document.querySelector('.notification').getAttribute('class');
-document.querySelector('.notification').setAttribute('class', 'notification');
+  //   document
+  //     .querySelector(".notification")
+  //     .classList.remove("notification_hidden");
+  document.querySelector(".notification").getAttribute("class");
+  document.querySelector(".notification").setAttribute("class", "notification");
 }
 
 function inputChaged(e) {
   if (e.code == "Enter") {
     submitForm();
   }
-};
+}
+
+// console.log(document.querySelector(".one").innerText);
+// console.log(document.querySelector(".one ~ div").innerText);
+
+console.log(document.querySelectorAll('.one')[0].innerText);
+console.log(document.querySelector("#two").innerText);
+console.log(document.querySelector("[user-id='4']").innerText);
