00a6214afb6c65be0d50d446f475c2d190bfc29f
Yury Kavalenka <yurykavalenka@gmail.com>
2023-12-24 14:23:03 +0100

    Exercise - managing this

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 'use strict'
 
-const audi = {
-    make: 'Audi',
-    model: 'A3',
-    damages: []
-};
-
-const carManipulation = {
-    addDamage(part, rate) {
-        this.damages.push({ part, rate });
-        console.log(`Добавить повреждения на ${this.make} ${this.model}`)
+function removePassword(reset) {
+    if (reset) {
+        this.password = undefined;
+    } else {
+        this.password = '1';
     }
 }
 
-const addDamageAudi = carManipulation.addDamage.bind(audi);
-addDamageAudi('Крыло', 3);
-console.log(audi);
+const user = {
+    login: 'a@a.pl',
+    password: '12345'
+};
 
+const resetUserPassword = removePassword.bind(user, true);
+resetUserPassword();
+console.log(user);
 
