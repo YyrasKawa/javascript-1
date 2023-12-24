5ae96151a0bb7b2e78123c9deae9bcd64d5f2bf8
Yury Kavalenka <yurykavalenka@gmail.com>
2023-12-24 14:04:33 +0100

    Call, apply

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
 'use strict'
 
-/* 
-	дополнить объект методами для получения имени:
-	- компании
-	- ceo
-	- сотрудника
-*/
-
-const company = {
-  name: "ООО Агро",
-  employees: [
-    {
-      name: "Света",
-      getName: function () {
-      return this.name;
-      }
-    },
-  ],
-  ceo: {
-    name: "Вася",
-    getName: function () {
-      return this.name;
-    },
-  },
-  getName: function () {
-    return this.name;
-  },
+const audi = {
+    make: 'Audi',
+    model: 'A3',
+    year: 2021,
+    damages: [],
+    addDamage(part, rate) {
+        console.log(`У авто ${this.make} ${this.model} добавлено: - повреждение ${part} со степенью ${rate}`
+        )
+        this.damages.push({
+            part,
+            rate
+        })
+    }
 };
 
-console.log(company.ceo.getName())
-console.log(company.employees.map(employee => employee.getName()));
-console.log(company.getName())
+audi.addDamage('Капот', 1);
+
+const bmw = {
+    make: 'BMW',
+    model: 'X5',
+    year: 2022,
+    damages: [],
+};
+
+bmw.addDamage = audi.addDamage;
+bmw.addDamage("Бампер", 2);
+
+const addDamageFunc = audi.addDamage;
+addDamageFunc.call(bmw, "Бампер", 2);
+
+addDamageFunc.apply(audi, ["Бампер", 2]);
 
 
 
