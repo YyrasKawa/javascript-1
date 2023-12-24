8682ea4ea538e3822e86ed1f7e5a47ff9b2bba04
Yury Kavalenka <yurykavalenka@gmail.com>
2023-12-24 15:02:11 +0100

    Exercise - working with closures

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 'use strict'
 
-function changeBalance() {
-    let balance = 0;
-    return function(sum) {
-        balance += sum;
-        console.log(`Баланс: ${balance}`)
+const userInfo = {
+    balance: 0,
+    operations: 0,
+    increse(sum) {
+        this.balance +=sum;
+        this.operations++;
     }
 }
 
-const change = changeBalance(); 
-change(100);
-change(-50);
-change(200);
+function user() {
+    const userObj = userInfo;
+    return function() {
+        return userObj;
+    }
+}
+
+const user1 = user();
+user1().increse(100);
+user1().increse(100);
+console.log(user1());
 
 
 
