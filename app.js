c8fe95bf5889834dc709dbf5fbf792c99d03e97d
Yury Kavalenka <yurykavalenka@gmail.com>
2023-12-24 14:15:35 +0100

    Bind

--- app.js
+++ app.js
@@ -3,34 +3,18 @@
 const audi = {
     make: 'Audi',
     model: 'A3',
-    year: 2021,
-    damages: [],
+    damages: []
+};
+
+const carManipulation = {
     addDamage(part, rate) {
-        console.log(`У авто ${this.make} ${this.model} добавлено: - повреждение ${part} со степенью ${rate}`
-        )
-        this.damages.push({
-            part,
-            rate
-        })
+        this.damages.push({ part, rate });
+        console.log(`Добавить повреждения на ${this.make} ${this.model}`)
     }
-};
+}
 
-audi.addDamage('Капот', 1);
-
-const bmw = {
-    make: 'BMW',
-    model: 'X5',
-    year: 2022,
-    damages: [],
-};
-
-bmw.addDamage = audi.addDamage;
-bmw.addDamage("Бампер", 2);
-
-const addDamageFunc = audi.addDamage;
-addDamageFunc.call(bmw, "Бампер", 2);
-
-addDamageFunc.apply(audi, ["Бампер", 2]);
-
+const addDamageAudi = carManipulation.addDamage.bind(audi);
+addDamageAudi('Крыло', 3);
+console.log(audi);
 
 
